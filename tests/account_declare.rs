d3eb2415d55b550d2b6dcfde92d7b5304404c81f
ICavlek <ivan.cavlek1403@gmail.com>
2024-08-01 09:35:32 +0200

    refactor: migrated huge constants to separate module

--- tests/account_declare.rs
+++ tests/account_declare.rs
@@ -1,14 +1,15 @@
 use beerus::gen::{
-    Address, BlockId, BlockTag, BroadcastedDeclareTxn, BroadcastedDeclareTxnV2,
-    BroadcastedDeclareTxnV2Type, BroadcastedDeclareTxnV2Version,
-    BroadcastedTxn, ContractClass, ContractClassEntryPointsByType, Felt, Rpc,
-    SierraEntryPoint,
+    Address, BlockId, BlockTag, BroadcastedDeclareTxn, BroadcastedTxn, Felt,
+    Rpc,
 };
-use common::api::{
-    setup_client_with_mock_starknet_node,
-    StarknetMatcher::{
-        ChainId, ClassError, ClassSuccess, EstimateFee, Nonce, SpecVersion,
+use common::{
+    api::{
+        setup_client_with_mock_starknet_node,
+        StarknetMatcher::{
+            ChainId, ClassError, ClassSuccess, EstimateFee, Nonce, SpecVersion,
+        },
     },
+    constants::declare_transaction_v2,
 };
 
 mod common;
@@ -71,37 +72,7 @@ async fn get_class_success() {
 
 #[tokio::test]
 async fn spec_version_estimate_fee() {
-    let declare_transaction = BroadcastedDeclareTxnV2 {
-        compiled_class_hash: Felt::try_new("0x0").unwrap(),
-        contract_class: ContractClass {
-            sierra_program: vec![Felt::try_new("0x1").unwrap()],
-            contract_class_version: "0.1.0".to_string(),
-            entry_points_by_type: ContractClassEntryPointsByType {
-                constructor: vec![SierraEntryPoint {
-                    selector: Felt::try_new("0x2").unwrap(),
-                    function_idx: 2,
-                }],
-                external: vec![
-                    SierraEntryPoint {
-                        selector: Felt::try_new("0x3").unwrap(),
-                        function_idx: 3,
-                    },
-                    SierraEntryPoint {
-                        selector: Felt::try_new("0x4").unwrap(),
-                        function_idx: 4,
-                    },
-                ],
-                l1_handler: vec![],
-            },
-            abi: Some("some_abi".to_string()),
-        },
-        max_fee: Felt::try_new("0x0").unwrap(),
-        nonce: Felt::try_new("0x0").unwrap(),
-        r#type: BroadcastedDeclareTxnV2Type::Declare,
-        signature: vec![Felt::try_new("0x5").unwrap()],
-        sender_address: Address(Felt::try_new("0x6").unwrap()),
-        version: BroadcastedDeclareTxnV2Version::V0x2,
-    };
+    let declare_transaction = declare_transaction_v2();
     let (client, _starknet_node) =
         setup_client_with_mock_starknet_node(vec![SpecVersion, EstimateFee])
             .await;
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,4 +1,5 @@
 pub mod api;
+pub mod constants;
 pub mod context;
 pub mod error;
 mod matchers;
